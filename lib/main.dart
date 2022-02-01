a162a57325a5d0d8ff72ee60812e6e7e8454ef63
Jorge Florido <51244465+Florido14@users.noreply.github.com>
2022-02-01 00:16:40 -0600

    feat: different changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,22 @@ import 'package:flutter/material.dart';
 import 'screens/screens.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+
+
  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Text('Hello World'),
       ),
-      home: const MyHomePage(title: 'home_page'),
     );
   }
 }
